8e124a400f243d585ee46dd3b027b14a63524085
Monique Nguyen <moniquehanguyen@gmail.com>
2020-09-08 18:31:01 -0500

    Completed exercises and studio.

---
+++ studio/Rocket.ts
@@ -0,0 +1,49 @@
+import { Astronaut } from './Astronaut'
+import { Cargo } from './Cargo'
+import { Payload } from './Payload'
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass(items: Payload[]) : number {
+        let totalMass: number = 0;
+        for (let i = 0; i < items.length; i++) {
+            totalMass = totalMass + items[i].massKg;
+        }
+        return totalMass
+    }
+
+    currentMassKg(): number {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    }
+
+    canAdd(item: Payload): boolean {
+        return this.currentMassKg() + item.massKg <= this.totalCapacityKg;
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut) === true) {
+            this.astronauts.push(astronaut);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
